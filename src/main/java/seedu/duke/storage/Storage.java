3ac28863ef6d0607f5e5deebcfa9f7b2d203fc4f
RNJH224 <e1354204@u.nus.edu>
2026-03-26 14:56:02 +0800

    log

--- src/main/java/seedu/duke/storage/Storage.java
+++ src/main/java/seedu/duke/storage/Storage.java
@@ -7,19 +7,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 import seedu.duke.module.Module;
 
 public class Storage {
+    private static final Logger logger = Logger.getLogger(Storage.class.getName());
     private static final String filePath = "data/duke.txt";
 
     public List<Module> load() throws IOException {
 
         File file = new File(filePath);
 
+        logger.info("Loading modules from file: " + filePath);
+
         file.getParentFile().mkdirs();
 
         if (!file.exists()) {
             file.createNewFile();
+            logger.warning("Save file not found. Created new file at " + filePath);
         }
 
         List<Module> modules = new ArrayList<>();
@@ -29,6 +37,8 @@ public class Storage {
             String line = scanner.nextLine();
 
             String[] parts = line.split("\\|");
+            logger.log(Level.FINE, "Reading line: {0}", line);
+
 
             String code = parts[0];
             int mc = Integer.parseInt(parts[1]);
